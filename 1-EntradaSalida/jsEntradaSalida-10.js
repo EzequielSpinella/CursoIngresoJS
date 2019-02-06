b9d693f8338b20b1c32a74818ef55f2ff3484370
alumno <alumno>
2019-02-06 19:10:38 -0300

    termine el ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,6 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
+{ 
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -3,7 +3,12 @@
 	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+/* var siempre igual nombre */
+{ 
+
+var nombre;
+	nombre = prompt("por favor ingrese su dato");
+	elNombre.value = nombre;
 
 }
 
